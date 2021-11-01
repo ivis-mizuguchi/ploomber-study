20c84b0a5d8366910093ca438fb415eac8997079
rino <rino>
2021-11-01 08:12:34 +0000

    a

--- scripts/get.py
+++ scripts/get.py
@@ -1,7 +1,7 @@
 # pandasで13105_20211_20211.csvをロードして最初の10行を表示する
 import pandas as pd
 pd.set_option('display.max_columns', 100)
-df = pd.read_csv('13105_20211_20211.csv', encoding='CP932')
+df = pd.read_csv('13105_20212_20212.csv', encoding='CP932')
 df.head(10)
 
 # + tags=["parameters"]
